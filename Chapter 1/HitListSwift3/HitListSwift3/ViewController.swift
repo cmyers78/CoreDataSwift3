e2167e6a598599ec543723da498a9e48b3fdfc7e
Chris Myers <cmyers78@gmail.com>
2016-10-20 13:03:13 -0400

    saving managed objects to persistent container code...next up--fetching those objects...

--- Chapter 1/HitListSwift3/HitListSwift3/ViewController.swift
+++ Chapter 1/HitListSwift3/HitListSwift3/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names : [String] = []
+    var people : [NSManagedObject] = []
+    
+   // var names : [String] = []
     
     
     override func viewDidLoad() {
@@ -35,11 +38,12 @@ class ViewController: UIViewController {
         let saveAction = UIAlertAction(title: "Save", style: .default) {
             [unowned self] action in
             
-            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+            guard let textField = alert.textFields?.first,
+                let nameToSave = textField.text else {
                 return
             }
             
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
             
         }
@@ -53,7 +57,35 @@ class ViewController: UIViewController {
         
         present(alert, animated: true)
         
+    }
     
+    func save(name : String) {
+        
+        // Step 1: acces the App Delegate file which contains the CD stuff...
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // Step 2: accessing the managed object context from the app delegate's persistent Container
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Step 3 : Create a new managed object and insert it into the managed object context---which lives in the persistent container.
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext) // links data model to entity definition
+        
+        let person = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        // tell the person managed object what attribute to access and set the information typed by the user to appropriate key name
+        person.setValue(name, forKey: "name")
+        
+        // save the managed object to the Core Data store and append the person to the array of managed objects (to load the tableView)
+        
+        do {
+          try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
 
 }
@@ -61,14 +93,17 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.names.count
+        return self.people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        
         
         return cell
     }
